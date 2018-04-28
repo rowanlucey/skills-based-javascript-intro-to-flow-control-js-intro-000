f3a94864392002b71dc14da1efb5546242f82377
Rowan Lucey <rslucey@gmail.com>
2018-04-28 01:02:58 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -26,11 +26,11 @@ function ternaryTeenager(age) {
   return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager"
 }
 
-function switchAge(age) {
-  switch(age) {
-    case 13 || 14 || 15 || 16 || 17 || 18 || 19
-      console.log("You are a teenager")
-    default:
-      console.log("You have an age")
-  }
-}
+// function switchAge(age) {
+//   switch(age) {
+//     case 13 || 14 || 15 || 16 || 17 || 18 || 19
+//       console.log("You are a teenager")
+//     default:
+//       console.log("You have an age")
+//   }
+// }
